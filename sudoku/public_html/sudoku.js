842fd8c4d3211d5e4c11a14ce8a32c05036accfc
MHBL-60 <MHBL-60@MHBL-MT-60>
2015-06-30 18:29:52 +0530

    updated.

--- sudoku/public_html/sudoku.js
+++ sudoku/public_html/sudoku.js
@@ -8,35 +8,82 @@ $(document).ready(function() {
         var cells = initCells(size);
         var done = []; // saves cell index as one-d array
         var left = []; // saves cell index as one-d array
+        var ignore = [];
         var curCell = [0,0];
         
         init();
-        build();
-        //output();
+        
+        while(left.length > 0) {
+            build();
+        }
+        output();
+        
         function build() {
             var c = chooseCell();
-            console.log(c);
             var n = 0;
+            var success = false;
+            curCell[0] = c.c_2d[0];
+            curCell[1] = c.c_2d[1];
+            
             for (var i = 0; i < size; i++) {
-//                n = xDigits[i];
-//                if (!conflict()) {
-//                    done.push()
-//                }
+                n = xDigits[i];
+                if (!conflict(c.c_2d, n)) {
+                    done.push(c.c_1d);
+                    left.splice(c.c_1d, 1);
+                    cells[c.c_2d[0]][c.c_2d[1]] = n;
+                    success = true;
+                    break;
+                }
+            }
+            
+            if (success === false) {
+                console.log('conflict');
+                backtrack();
+//                done.push(0);
+//                left.splice(c.c_1d, 1);
             }
         }
         
-        function conlict(c_2d) {
-            var row = function() {
+        function conflict(c_2d, n) {
+            var rowFlag = false;
+            var colFlag = false;
+            console.log(c_2d);
+            console.log(cells[c_2d[0]]);
+            var rowWise = function() {
                 for (var i = 0; i < size; i++) {
-                    
+                    if (cells[i][c_2d[1]] == n) {
+                        rowFlag = true;
+                        break;
+                    }
                 }
             }
+            
+            var colWise = function() {
+                for (var i = 0; i < size; i++) {
+                    if (cells[c_2d[0]][i] == n) {
+                        colFlag = true;
+                        break;
+                    }
+                }
+            }
+            
+            rowWise();
+            colWise();
+            return (rowFlag || colFlag);
+        }
+        
+        function backtrack() {
+            var doneLastCell = done[done.length - 1];
+            //ignore[doneLastCell] = n;
+            left.push(doneLastCell);
+            done.splice((done.length - 1), 1);
         }
         
         function init() {
             var max1dIndex = size * size - 1;
             for (var i = 0; i <= max1dIndex; i++) {
                 left.push(i);
+                ignore.push(0);
             }
         }
     
@@ -101,6 +148,10 @@ $(document).ready(function() {
              var cl_ = oneDIndex % size;
              
              return [rw_, cl_];
+        }
+        
+        function get1DIndex(twoDIndex) {
+            
         }
         
         function chooseCell() {
