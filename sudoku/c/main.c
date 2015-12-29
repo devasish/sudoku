b6d56efe4c4bf43cda8e1a5c567b71c13c63653d
devasish <dev.achieve@gmail.com>
2015-12-29 13:06:28 +0530

    changes

---
+++ sudoku/c/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define SIZE 9
+
+//int result[SIZE][SIZE];
+//
+//void init() {
+//    int i, j;
+//    for (i = 0; i < SIZE; i++) {
+//        for (j = 0; j < SIZE; j++) {
+//            result[i][j] = 0;
+//            printf("%d\t", result[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+
+int main() {
+    //init();
+    int a[SIZE];
+    int **arr, i, j;
+    arr = (int**) malloc(sizeof(int) * SIZE * SIZE);
+
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            arr[i][j] = 0;
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return EXIT_SUCCESS;
+}
+
+
